eb30daa70112b7be4f4bedc931598b47edf5005a
bayurokhmatullahhh <bayuurokhmatullah@gmail.com>
2025-01-08 15:47:44 +0700

    update frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./content/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
 );
